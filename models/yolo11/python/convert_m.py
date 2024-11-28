6c54ec8f53c1f5f90ff2eebddc3ba67d23cec627
xujg <1061904205@qq.com>
2024-11-28 10:22:43 +0800

    add yolo11n.rknn

--- models/yolo11/python/convert_m.py
+++ models/yolo11/python/convert_m.py
@@ -2,7 +2,7 @@ import sys
 from rknn.api import RKNN
 
 DATASET_PATH = '../../../datasets/COCO/coco_subset_20.txt'
-DEFAULT_RKNN_PATH = '../model/yolo11.rknn'
+DEFAULT_RKNN_PATH = '../model/yolo11_n.rknn'
 DEFAULT_QUANT = False
 
 def parse_arg():
